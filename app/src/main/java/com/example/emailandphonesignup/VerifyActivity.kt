b36b3b6650438def223137d70eeb7f7a90338161
SamuelNoye <samuel.noye@amalitech.org>
2021-11-15 13:44:09 +0000

    intial commit

---
+++ app/src/main/java/com/example/emailandphonesignup/VerifyActivity.kt
@@ -0,0 +1,35 @@
+package com.example.emailandphonesignup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+
+class VerifyActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        auth= FirebaseAuth.getInstance()
+        var currentUser=auth.currentUser
+
+//        Reference
+        val logout=findViewById<Button>(R.id.idLogout)
+
+        if(currentUser==null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+        logout.setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+    }
+}
